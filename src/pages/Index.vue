83e54561c5f915de8bc92537bcba1f066d6db83b
Billy <billysean.tj@gmail.com>
2021-12-24 18:20:26 +0700

    add wishes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,151 +9,225 @@
         />
       </div>
       <div class="flex flex-col">
-          <div class="flex overflow-x-scroll pb-10 hide-scroll-bar">
-            <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10">
-              <div class="inline-block px-3">
+        <div class="flex overflow-x-scroll pb-10 hide-scroll-bar">
+          <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10">
+            <div class="inline-block px-3">
+              <div
+                class="
+                  w-48
+                  h-64
+                  max-w-xs
+                  overflow-hidden
+                  rounded-lg
+                  shadow-md
+                  bg-white
+                  hover:shadow-xl
+                  transition-shadow
+                  duration-300
+                  ease-in-out
+                  bg-opacity-70
+                "
+              >
                 <div
-                  class="
-                    w-48
-                    h-64
-                    max-w-xs
-                    overflow-hidden
-                    rounded-lg
-                    shadow-md
-                    bg-white
-                    hover:shadow-xl
-                    transition-shadow
-                    duration-300
-                    ease-in-out
-                  "
+                  class="container px-5 py-5 mx-auto text-center image-center"
                 >
-                <div class="container px-5 py-5 mx-auto text-center">
-                  <h1>William Sugianto</h1>
+                  <img src="../layouts/wilsug.png" class="justify-center" />
+                  <div
+                    class="
+                      flex flex-col
+                      items-center
+                      text-center
+                      justify-center
+                    "
+                  >
+                    <h2
+                      class="font-medium title-font mt-4 text-gray-900 text-lg"
+                    >
+                      William Sugianto
+                    </h2>
+                    <div
+                      class="
+                        w-12
+                        h-1
+                        bg-gradient-to-r
+                        from-pink-200
+                        to-blue-200
+                        rounded
+                        mt-2
+                        mb-4
+                      "
+                    ></div>
+                    <p class="text-base italic">"Nama William kayanya bagus"</p>
+                  </div>
                 </div>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div
+                class="
+                  w-48
+                  h-64
+                  max-w-xs
+                  overflow-hidden
+                  rounded-lg
+                  shadow-md
+                  bg-white
+                  hover:shadow-xl
+                  transition-shadow
+                  duration-300
+                  ease-in-out
+                  bg-opacity-70
+                "
+              >
+                <div
+                  class="container px-5 py-5 mx-auto text-center image-center"
+                >
+                  <img src="../layouts/clinton.png" class="justify-center" />
+                  <div
+                    class="
+                      flex flex-col
+                      items-center
+                      text-center
+                      justify-center
+                    "
+                  >
+                    <h2
+                      class="font-medium title-font mt-4 text-gray-900 text-lg"
+                    >
+                      Clinton Sean
+                    </h2>
+                    <div
+                      class="
+                        w-12
+                        h-1
+                        bg-gradient-to-r
+                        from-pink-200
+                        to-blue-200
+                        rounded
+                        mt-2
+                        mb-4
+                      "
+                    ></div>
+                    <p class="text-base italic">
+                      "Goodluck and keep in touch! Until we meet again!"
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div class="inline-block px-3">
+            </div>
+            <div class="inline-block px-3">
+              <div
+                class="
+                  w-48
+                  h-64
+                  max-w-xs
+                  overflow-hidden
+                  rounded-lg
+                  shadow-md
+                  bg-white
+                  hover:shadow-xl
+                  transition-shadow
+                  duration-300
+                  ease-in-out
+                  bg-opacity-70
+                "
+              >
                 <div
-                  class="
-                    w-48
-                    h-64
-                    max-w-xs
-                    overflow-hidden
-                    rounded-lg
-                    shadow-md
-                    bg-white
-                    hover:shadow-xl
-                    transition-shadow
-                    duration-300
-                    ease-in-out
-                  "
-                ></div>
+                  class="container px-5 py-5 mx-auto text-center image-center"
+                >
+                  <img
+                    src="../layouts/billy-monic.png"
+                    class="justify-center"
+                  />
+                  <div
+                    class="
+                      flex flex-col
+                      items-center
+                      text-center
+                      justify-center
+                    "
+                  >
+                    <h2
+                      class="font-medium title-font mt-4 text-gray-900 text-lg"
+                    >
+                      Billy Sean
+                    </h2>
+                    <div
+                      class="
+                        w-12
+                        h-1
+                        bg-gradient-to-r
+                        from-pink-200
+                        to-blue-200
+                        rounded
+                        mt-2
+                        mb-4
+                      "
+                    ></div>
+                    <p class="text-base italic">
+                      "Semoga lilin ini bisa nutupin kalo amel males sapu-pel"
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div class="inline-block px-3">
+            </div>
+            <div class="inline-block px-3">
+              <div
+                class="
+                  w-48
+                  h-64
+                  max-w-xs
+                  overflow-hidden
+                  rounded-lg
+                  shadow-md
+                  bg-white
+                  hover:shadow-xl
+                  transition-shadow
+                  duration-300
+                  ease-in-out
+                  bg-opacity-70
+                "
+              >
                 <div
-                  class="
-                    w-48
-                    h-64
-                    max-w-xs
-                    overflow-hidden
-                    rounded-lg
-                    shadow-md
-                    bg-white
-                    hover:shadow-xl
-                    transition-shadow
-                    duration-300
-                    ease-in-out
-                  "
-                ></div>
-              </div>
-              <div class="inline-block px-3">
-                <div
-                  class="
-                    w-48
-                    h-64
-                    max-w-xs
-                    overflow-hidden
-                    rounded-lg
-                    shadow-md
-                    bg-white
-                    hover:shadow-xl
-                    transition-shadow
-                    duration-300
-                    ease-in-out
-                  "
-                ></div>
-              </div>
-              <div class="inline-block px-3">
-                <div
-                  class="
-                    w-48
-                    h-64
-                    max-w-xs
-                    overflow-hidden
-                    rounded-lg
-                    shadow-md
-                    bg-white
-                    hover:shadow-xl
-                    transition-shadow
-                    duration-300
-                    ease-in-out
-                  "
-                ></div>
-              </div>
-              <div class="inline-block px-3">
-                <div
-                  class="
-                    w-48
-                    h-64
-                    max-w-xs
-                    overflow-hidden
-                    rounded-lg
-                    shadow-md
-                    bg-white
-                    hover:shadow-xl
-                    transition-shadow
-                    duration-300
-                    ease-in-out
-                  "
-                ></div>
-              </div>
-              <div class="inline-block px-3">
-                <div
-                  class="
-                    w-48
-                    h-64
-                    max-w-xs
-                    overflow-hidden
-                    rounded-lg
-                    shadow-md
-                    bg-white
-                    hover:shadow-xl
-                    transition-shadow
-                    duration-300
-                    ease-in-out
-                  "
-                ></div>
-              </div>
-              <div class="inline-block px-3">
-                <div
-                  class="
-                    w-48
-                    h-64
-                    max-w-xs
-                    overflow-hidden
-                    rounded-lg
-                    shadow-md
-                    bg-white
-                    hover:shadow-xl
-                    transition-shadow
-                    duration-300
-                    ease-in-out
-                  "
-                ></div>
+                  class="container px-5 py-5 mx-auto text-center image-center"
+                >
+                  <img src="../layouts/hans-melisa.png" class="justify-center" />
+                  <div
+                    class="
+                      flex flex-col
+                      items-center
+                      text-center
+                      justify-center
+                    "
+                  >
+                    <h2
+                      class="font-medium title-font mt-4 text-gray-900 text-lg"
+                    >
+                      Hans William & Melisa
+                    </h2>
+                    <div
+                      class="
+                        w-12
+                        h-1
+                        bg-gradient-to-r
+                        from-pink-200
+                        to-blue-200
+                        rounded
+                        mt-2
+                        mb-4
+                      "
+                    ></div>
+                    <p class="text-base italic">
+                      "Jangan lupa sama kita
+                      <span class="not-italic">&#128528;&#128520;</span>"
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
     </section>
   </Layout>
 </template>
@@ -177,5 +251,8 @@ export default {
 }
 .hide-scroll-bar::-webkit-scrollbar {
   display: none;
+}
+.image-center {
+  text-align: -webkit-center;
 }
 </style>
