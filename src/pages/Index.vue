c52b8a52ff7fc452e871d88f2aaeea2b3c73092b
Billy <billysean.tj@gmail.com>
2021-12-24 16:59:57 +0700

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,181 @@
+<template>
+  <Layout>
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+        <img
+          class="lg:h-48 md:h-36 w-full object-cover object-center"
+          src="../layouts/newlife.png"
+          alt="blog"
+        />
+      </div>
+      <div class="flex flex-col">
+          <div class="flex overflow-x-scroll pb-10 hide-scroll-bar">
+            <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10">
+              <div class="inline-block px-3">
+                <div
+                  class="
+                    w-48
+                    h-64
+                    max-w-xs
+                    overflow-hidden
+                    rounded-lg
+                    shadow-md
+                    bg-white
+                    hover:shadow-xl
+                    transition-shadow
+                    duration-300
+                    ease-in-out
+                  "
+                >
+                <div class="container px-5 py-5 mx-auto text-center">
+                  <h1>William Sugianto</h1>
+                </div>
+                </div>
+              </div>
+              <div class="inline-block px-3">
+                <div
+                  class="
+                    w-48
+                    h-64
+                    max-w-xs
+                    overflow-hidden
+                    rounded-lg
+                    shadow-md
+                    bg-white
+                    hover:shadow-xl
+                    transition-shadow
+                    duration-300
+                    ease-in-out
+                  "
+                ></div>
+              </div>
+              <div class="inline-block px-3">
+                <div
+                  class="
+                    w-48
+                    h-64
+                    max-w-xs
+                    overflow-hidden
+                    rounded-lg
+                    shadow-md
+                    bg-white
+                    hover:shadow-xl
+                    transition-shadow
+                    duration-300
+                    ease-in-out
+                  "
+                ></div>
+              </div>
+              <div class="inline-block px-3">
+                <div
+                  class="
+                    w-48
+                    h-64
+                    max-w-xs
+                    overflow-hidden
+                    rounded-lg
+                    shadow-md
+                    bg-white
+                    hover:shadow-xl
+                    transition-shadow
+                    duration-300
+                    ease-in-out
+                  "
+                ></div>
+              </div>
+              <div class="inline-block px-3">
+                <div
+                  class="
+                    w-48
+                    h-64
+                    max-w-xs
+                    overflow-hidden
+                    rounded-lg
+                    shadow-md
+                    bg-white
+                    hover:shadow-xl
+                    transition-shadow
+                    duration-300
+                    ease-in-out
+                  "
+                ></div>
+              </div>
+              <div class="inline-block px-3">
+                <div
+                  class="
+                    w-48
+                    h-64
+                    max-w-xs
+                    overflow-hidden
+                    rounded-lg
+                    shadow-md
+                    bg-white
+                    hover:shadow-xl
+                    transition-shadow
+                    duration-300
+                    ease-in-out
+                  "
+                ></div>
+              </div>
+              <div class="inline-block px-3">
+                <div
+                  class="
+                    w-48
+                    h-64
+                    max-w-xs
+                    overflow-hidden
+                    rounded-lg
+                    shadow-md
+                    bg-white
+                    hover:shadow-xl
+                    transition-shadow
+                    duration-300
+                    ease-in-out
+                  "
+                ></div>
+              </div>
+              <div class="inline-block px-3">
+                <div
+                  class="
+                    w-48
+                    h-64
+                    max-w-xs
+                    overflow-hidden
+                    rounded-lg
+                    shadow-md
+                    bg-white
+                    hover:shadow-xl
+                    transition-shadow
+                    duration-300
+                    ease-in-out
+                  "
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+
+.hide-scroll-bar {
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+.hide-scroll-bar::-webkit-scrollbar {
+  display: none;
+}
+</style>
